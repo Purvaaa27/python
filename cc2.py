eaff0eb55f9d8e919b035979dea0237724bd33d0
Purvaaa27 <118345044+Purvaaa27@users.noreply.github.com>
2025-02-26 11:32:22 +0530

    Create cc2.py

---
+++ cc2.py
@@ -0,0 +1,12 @@
+print("Check weather the number is odd or even")
+
+a  = int(input("Enter the number:"))
+
+if a%2  == 0:
+    print("The number is postive")
+elif a == 0:
+    print("The number is zero")
+else:
+    print("The number is negative")
+
+print("Hope you enjoyed it")
